bf3bbdab3cebff4fa15b15e95fc22dec7801c06f
guiyumin <guiyumin@protonmail.com>
2024-10-10 21:07:32 -0700

    fix: html

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { Hono } from "hono";
+import { html } from "hono/html";
 
 const app = new Hono();
 
+const HomePage = () => {
+  return html`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hello Hono!</title>
+      </head>
+      <body>
+        Hello Hono!!!
+      </body>
+    </html>
+  `;
+};
+
 // TODO: for this route, return html
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.html(HomePage());
 });
 
 export default app;
